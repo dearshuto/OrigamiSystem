8899e0841f174f20fb2440df3ee08950a71c6283
Shuto Shikama <shuto.shikama@fj.ics.keio.ac.jp>
2017-02-07 19:08:44 +0900

    [add] Add the new class

---
+++ include/origami_system/algorithm/straight_skelton.hpp
@@ -0,0 +1,14 @@
+//
+//  straight_skelton.hpp
+//  OrigamiSystem
+//
+//  Created by Shuto Shikama on 2017/02/07.
+//
+//
+
+#ifndef straight_skelton_hpp
+#define straight_skelton_hpp
+
+#include <stdio.h>
+
+#endif /* straight_skelton_hpp */
---
+++ src/algorithm/straight_skelton.cpp
@@ -0,0 +1,9 @@
+//
+//  straight_skelton.cpp
+//  OrigamiSystem
+//
+//  Created by Shuto Shikama on 2017/02/07.
+//
+//
+
+#include "straight_skelton.hpp"
